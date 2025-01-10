da7c0d0c82035bdb330af5f1c343083ef062d7bc
axwvvvvv <galiyevamirbek84@gmail.com>
2025-01-10 16:41:54 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Header from './components/Header/Header'
 import './App.css'
+import Header from './components/Header/Header'
 
 const App = () => {
   return (
     <>
-    <Header/>
+      <Header/>
     </>
   )
 }
